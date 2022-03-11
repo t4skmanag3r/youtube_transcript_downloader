f2f9f9e73e219b6d1fc4c2c85656abd613b96cea
Edvinas Adomaitis <t4skmanag3r@gmail.com>
2022-03-11 17:53:52 +0200

    made placeholder functions

---
+++ yt_transcript_downloader.py
@@ -0,0 +1,15 @@
+import requests as req
+import json
+import re
+
+
+def _GetSerializedShareEntity(url):
+    pass
+
+
+def _GetInnertubeApiKey(url):
+    pass
+
+
+def GetTranscript(url):
+    pass
