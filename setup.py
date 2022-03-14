d2c5a055d02f74c2737fa1aa4081534c71dd6942
Edvinas Adomaitis <t4skmanag3r@gmail.com>
2022-03-14 12:13:54 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 DESCRIPTION = "A package for retrieving transcripts from youtube"
 
 setup(
